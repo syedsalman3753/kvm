eec2124ac21c9a3f5aee30cb418152f9db294a2a
123iris <syedsalman041997@gmail.com>
2021-03-02 14:52:12 +0530

    #1 All code save under dirs based on there work

--- code/infra/2-CloneVms/vmClone.sh
+++ code/infra/2-CloneVms/vmClone.sh
@@ -5,10 +5,10 @@ vmShutdown(){
   vm=$1
   status=$(virsh list --all | grep -w "$vm" | awk '{print $3}')
  	if [[ "$status" == "shut" ]]; then
-	  echo -e "$(tput setaf 2)\n    Domain $vm Shutdown $(tput sgr0)"
+	  echo -e "$(tput setaf 2)\n\tDomain $vm Shutdown $(tput sgr0)"
 	  return
  	else
-    echo -en "    "
+    echo -en "\n\t"
     tput setaf 10
     virsh shutdown $vm;
     tput sgr0
@@ -21,13 +21,13 @@ chkVmStatus() {
   c=0
   while [ $c -lt 10 ]; do
     vmStatus=$(virsh list --all | grep -w "$vm" | awk '{print $3$4}')
-    echo -e "$(tput setaf 3)    Trying to Get Status for $chkVm machine : $(tput sgr0)  $vmStatus $c ";
+    echo -e "$(tput setaf 3)\tTrying to Get Status for $chkVm machine : $(tput sgr0)  $vmStatus $c ";
 
     # To check user connectivity with vm's.
     result=$(ping -qc1 $chkVm 2>&1)
     status=$(echo $?)
     if [[ $status -gt 0 ]]; then
-      echo -e "$(tput setaf 2)    $chkVm machine is now Shutdown $(tput sgr0) "
+      echo -e "$(tput setaf 2)\t$chkVm machine is now Shutdown $(tput sgr0) "
       return ;
     fi
     ((c++))
@@ -54,13 +54,17 @@ echo    # (optional) move to a new line
 
 if ! [[ $REPLY =~ ^[Yy]$ ]]; then
     echo " Vm Clone operation not performed; EXITING;";
+    exit 1
 fi
 
 # shutdown machine
 echo -n "$(tput setaf 9) [shutdown-Machine] $(tput sgr 0)"
-vmShutdown $srcVm          		   # calling vmShutdown function 
+vmShutdown $srcVm          		    # calling vmShutdown function 
 chkVmStatus $srcVm        	 		# calling vmStatus function
 
+sleep 10
+
+echo -n "$(tput setaf 9) [clone-Machine] $(tput sgr 0)"
 echo -e "\n Clone $destVm from $srcVm"
 sudo virt-clone --connect qemu:///system  --original $srcVm  --name $destVm  --auto-clone
 	
